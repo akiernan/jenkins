75fbb4cfa9d6a4b7e9594052764346f9434bcfd5
Alex Kiernan <alex.kiernan@bgch.co.uk>
2016-07-13 10:58:19 +0100

    Ensure sources appear before update (and then trigger packages)

--- puppet/environments/vagrant/modules/vagrant/manifests/init.pp
+++ puppet/environments/vagrant/modules/vagrant/manifests/init.pp
@@ -2,11 +2,9 @@ class vagrant {
   include apt
 
   Class['apt::update'] -> Package <| provider == 'apt' |>
-
+  Apt::Source <| |> ~> Class['apt::update'] -> Package <| |>
 
   $packages = lookup('packages', Array[String], 'unique', [])
-
-
   package { $packages:
     ensure => present
   }
